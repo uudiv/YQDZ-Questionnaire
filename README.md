96ea0c53930f1e6422076cc2bd141162d57d7aee
zxz <690792862@qq.com>
2018-12-18 22:40:24 +0800

    加入Vue框架，搭建基础结构，已完成两页

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # YQDZ-Questionnaire
 一汽-大众 调查问卷
+
+## 技术选型
+	-HTML5
+	-CSS3
+	-Less
+	-MUI
+	-Swiper
+	-Vue
+	
+## 主要内容
+	-调查问卷
+	-收集用户信息
+	-抽奖活动
+	-推广
+	
--- js/index.js
+++ js/index.js
@@ -1,47 +1,61 @@
-$(function() {
-	//音乐
-	/*function audioAutoPlay(id) {
-		var audio = document.getElementById(id);
-		audio.play();
-		document.addEventListener("WeixinJSBridgeReady", function() {
+var vm = new Vue({
+	el: '#app',
+	data: {
+		name:'',
+		phone:''
+	},
+	methods: {
+
+	},
+	//初始化完成
+	created() {
+
+	},
+	//页面挂载到dom上
+	mounted() {
+		//音乐
+		/*function audioAutoPlay(id) {
+			var audio = document.getElementById(id);
 			audio.play();
-		}, false);
-	}
-	audioAutoPlay('musicStar');*/
-
-	//swiper-container
-	var mySwiper = new Swiper('.swiper-container', {
-		autoplay: false,
-		direction: 'horizontal',
-		loop: false,
-		noSwiping: true,
-		initialSlide: 0,
-		// 如果需要前进后退按钮
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
-		on: {
-			init: function() {
-				swiperAnimateCache(this); //隐藏动画元素 
-				swiperAnimate(this); //初始化完成开始动画
-			},
-			slideChangeTransitionEnd: function() {
-				swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
-				//this.slides.eq(this.activeIndex).find('.ani').removeClass('ani'); //动画只展现一次，去除ani类名
-			}
+			document.addEventListener("WeixinJSBridgeReady", function() {
+				audio.play();
+			}, false);
 		}
-	});
+		audioAutoPlay('musicStar');*/
 
-	//scroll
-	mui('.mui-scroll-wrapper').scroll({
-		scrollY: true, //是否竖向滚动
-		scrollX: false, //是否横向滚动
-		startX: 0, //初始化时滚动至x
-		startY: 0, //初始化时滚动至y
-		indicators: false, //是否显示滚动条
-		deceleration: 0.0006, //阻尼系数,系数越小滑动越灵敏
-		bounce: true //是否启用回弹
-	})
+		//swiper-container
+		var mySwiper = new Swiper('.swiper-container', {
+			autoplay: false,
+			direction: 'horizontal',
+			loop: false,
+			noSwiping: true,
+			initialSlide: 0,
+			// 如果需要前进后退按钮
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
+			},
+			on: {
+				init: function() {
+					swiperAnimateCache(this); //隐藏动画元素 
+					swiperAnimate(this); //初始化完成开始动画
+				},
+				slideChangeTransitionEnd: function() {
+					swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+					//this.slides.eq(this.activeIndex).find('.ani').removeClass('ani'); //动画只展现一次，去除ani类名
+				}
+			}
+		});
 
+		//scroll
+		mui('.mui-scroll-wrapper').scroll({
+			scrollY: true, //是否竖向滚动
+			scrollX: false, //是否横向滚动
+			startX: 0, //初始化时滚动至x
+			startY: 0, //初始化时滚动至y
+			indicators: false, //是否显示滚动条
+			deceleration: 0.0006, //阻尼系数,系数越小滑动越灵敏
+			bounce: true //是否启用回弹
+		})
+	}
 })
